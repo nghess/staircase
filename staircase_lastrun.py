a02471b83330c633348144be65c90728375ec98a
Integrative Perception Lab Admin <ipladmin@PSY-200061.ad.uoregon.edu>
2023-05-16 14:54:31 -0700

    psychopy 2023.1.1

--- staircase_lastrun.py
+++ staircase_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.1.1),
-    on May 16, 2023, at 14:01
+    on May 16, 2023, at 14:52
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -241,6 +241,8 @@ class Generate:
                 os.makedirs(folder)
             for i in range(self.size):
                 cv2.imwrite(f"{folder}{self.beta}_{self.seed}_{i:03d}.png", self.pattern[i, :, :]*255)
+# Run 'Before Experiment' code from increase_beta
+performance = []
 
 
 # Ensure that relative paths start from the same directory as this script
@@ -262,7 +264,7 @@ expInfo['expName'] = expName
 expInfo['psychopyVersion'] = psychopyVersion
 
 # Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
-filename = _thisDir + os.sep + u'test_data'
+filename = _thisDir + os.sep + u'data/test_data'
 
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
@@ -329,8 +331,24 @@ image_r = visual.ImageStim(
 mouse = event.Mouse(win=win)
 x, y = [None, None]
 mouse.mouseClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='',
+    font='Open Sans',
+    pos=(0, -.33), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=-4.0);
 
 # --- Initialize components for Routine "adjust_beta" ---
+
+# --- Initialize components for Routine "Debrief" ---
+text_2 = visual.TextStim(win=win, name='text_2',
+    text='Thank You!\n',
+    font='Open Sans',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -379,6 +397,12 @@ for thisStaircase in staircase:
         correct = image_l
     else:
         correct = image_r
+    
+    # If answers fall below 50% accuracy, end block
+    if np.sum(performance[-4:-1]) > 1:
+        print("failure")
+        nReps = 0
+        #continueRoutine = False
     image_l.setImage('stimuli/fractal_left.png')
     image_r.setImage('stimuli/fractal_right.png')
     # setup some python lists for storing info about the mouse
@@ -392,7 +416,7 @@ for thisStaircase in staircase:
     mouse.clicked_name = []
     gotValidClick = False  # until a click is received
     # keep track of which components have finished
-    trialComponents = [image_l, image_r, mouse]
+    trialComponents = [image_l, image_r, mouse, text]
     for thisComponent in trialComponents:
         thisComponent.tStart = None
         thisComponent.tStop = None
@@ -481,6 +505,8 @@ for thisStaircase in staircase:
             mouse.tStart = t  # local t and not account for scr refresh
             mouse.tStartRefresh = tThisFlipGlobal  # on global time
             win.timeOnFlip(mouse, 'tStartRefresh')  # time at next scr refresh
+            # add timestamp to datafile
+            thisExp.addData('mouse.started', t)
             # update status
             mouse.status = STARTED
             mouse.mouseClock.reset()
@@ -515,8 +541,26 @@ for thisStaircase in staircase:
                     mouse.midButton.append(buttons[1])
                     mouse.rightButton.append(buttons[2])
                     mouse.time.append(mouse.mouseClock.getTime())
-                    
-                    continueRoutine = False  # end routine on response
+                    if gotValidClick:
+                        continueRoutine = False  # end routine on response
+        
+        # *text* updates
+        
+        # if text is starting this frame...
+        if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+            # keep track of start time/frame for later
+            text.frameNStart = frameN  # exact frame index
+            text.tStart = t  # local t and not account for scr refresh
+            text.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+            # update status
+            text.status = STARTED
+            text.setAutoDraw(True)
+        
+        # if text is active this frame...
+        if text.status == STARTED:
+            # update params
+            text.setText(performance, log=False)
         
         # check for quit (typically the Esc key)
         if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
@@ -562,12 +606,14 @@ for thisStaircase in staircase:
     count += 1
     # Nudge betas toward target
     converged_numbers = converge_numbers(betas, target, count)
-    
+    # Name and store beta values
     beta_hi = max(converged_numbers)
     beta_lo = min(converged_numbers)
+    staircase.addData('beta_hi', beta_hi)
+    staircase.addData('beta_lo', beta_lo)
     
-    print(beta_hi)
-    print(beta_lo)
+    # Append most recent answer to performance list 
+    performance.append(mouse.corr[0])
     # keep track of which components have finished
     adjust_betaComponents = []
     for thisComponent in adjust_betaComponents:
@@ -622,6 +668,80 @@ for thisStaircase in staircase:
     
 # completed 100.0 repeats of 'staircase'
 
+
+# --- Prepare to start Routine "Debrief" ---
+continueRoutine = True
+# update component parameters for each repeat
+# keep track of which components have finished
+DebriefComponents = [text_2]
+for thisComponent in DebriefComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+frameN = -1
+
+# --- Run Routine "Debrief" ---
+routineForceEnded = not continueRoutine
+while continueRoutine:
+    # get current time
+    t = routineTimer.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text_2* updates
+    
+    # if text_2 is starting this frame...
+    if text_2.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text_2.frameNStart = frameN  # exact frame index
+        text_2.tStart = t  # local t and not account for scr refresh
+        text_2.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text_2, 'tStartRefresh')  # time at next scr refresh
+        # add timestamp to datafile
+        thisExp.timestampOnFlip(win, 'text_2.started')
+        # update status
+        text_2.status = STARTED
+        text_2.setAutoDraw(True)
+    
+    # if text_2 is active this frame...
+    if text_2.status == STARTED:
+        # update params
+        pass
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+        if eyetracker:
+            eyetracker.setConnectionState(False)
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        routineForceEnded = True
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in DebriefComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# --- Ending Routine "Debrief" ---
+for thisComponent in DebriefComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# the Routine "Debrief" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 
 # --- End experiment ---
 # Flip one final time so any remaining win.callOnFlip() 
